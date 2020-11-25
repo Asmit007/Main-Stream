4e48131f3238d0857e37b4862cca9d81302183b0
Saurabh-del <gautamsaurabh199@gmail.com>
2020-11-25 16:16:11 +0530

    try

--- Stream-project-main/firstapp/models.py
+++ Stream-project-main/firstapp/models.py
@@ -9,6 +9,7 @@ class MovieData(models.Model):
     desc = models.TextField(max_length=500, default="")
     platform = models.CharField(max_length=500)
     poster = models.ImageField(upload_to = 'images/')
+    #hello 
 
     def __str__(self):
         return self.title
