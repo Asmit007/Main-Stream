1f8e1dbc7403deaa19abfe62217b1e87229828d0
Asmit007 <Asmit007@users.noreply.github.com>
2020-11-28 19:05:51 +0530

    new links + templates added

--- Stream-project-main/firstapp/urls.py
+++ Stream-project-main/firstapp/urls.py
@@ -7,4 +7,10 @@ path('movies/', views.MoviePage, name="Movies" ) ,
 path('tvseries/',views.TvseriesPage,name="TV") ,
 path('login/',views.Login,name='Login'),
 path('register/',views.SignUP,name='Sup'),
+
+path('movcomingsoon/',views.MovComingSoon,name='MCS'), 
+path('movintheater/',views.MovInTheater,name='MIT'),
+path('movmostviewed/',views.MovMostView,name='MMV'),
+path('movtoprated/',views.MovTopRated,name='MTR'),
+
 ]
--- Stream-project-main/firstapp/views.py
+++ Stream-project-main/firstapp/views.py
@@ -3,14 +3,24 @@ from django.http import HttpResponse
 from .models import MovieData, SeriesData
 # Create your views here.
 
+def MovComingSoon(request):
+    return render(request,'MovComingSoon.html')
+
+def MovInTheater(request):
+    return render(request,'MovInTheater.html')
+
+def MovMostView(request):
+    return render(request,'MovMostView.html')
+
+def MovTopRated(request):
+    return render(request,'MovTopRated.html') 
+
 def HomePage(request):
     obj = SeriesData.objects.all()
     obj2 = MovieData.objects.all() 
     return render(request,'Homepage.html',{'obj':obj , 'obj2':obj2}) 
 
 def MoviePage(request):
-    # obj = MovieData.objects.all()
-    # print(obj)
     return render(request,'Movies.html') 
 
 def TvseriesPage(request):
